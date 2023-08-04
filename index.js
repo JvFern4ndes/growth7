bfa06c262d7b78fb0f845b66e194ff8299870593
João Victor <jvfernandes.contato@gmail.com>
2023-08-04 01:45:22 -0300

    feat: implements utility function to remove texts from estimated time

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const jobs = require('./input.js');
 
+function transformTimeStringToNumber(timeString) {
+  const timeParts = timeString.split(' ');
+  const hours = parseInt(timeParts[0]);
+  return hours;
+}
+
 function getDayJobs(jobs) {
   const groupedJobs = [[]];
 
   for (const job of jobs) {
     let addedToGroup = false;
+    const jobTime = transformTimeStringToNumber(job['Tempo estimado']);
 
     for (let i = 0; i < groupedJobs.length; i++) {
       const group = groupedJobs[i];
-      const totalTimeGroup = group.reduce((total, j) => total + j['Tempo estimado'], 0);
+      const totalTimeGroup = group.reduce((total, j) => total + transformTimeStringToNumber(j['Tempo estimado']), 0);
 
-      if (totalTimeGroup + job['Tempo estimado'] <= 8) {
+      if (totalTimeGroup + jobTime <= 8) {
         group.push(job);
         addedToGroup = true;
         break;
--- input.js
+++ input.js
@@ -3,48 +3,48 @@ module.exports = [
     "ID": 1,
 		"Descrição": "Implantação de API", 
 		"Data Máxima de conclusão": '2023-08-04 12:00:00', 
-		"Tempo estimado": 8
+		"Tempo estimado": '8 horas'
   },
   {
     "ID": 2,
 		"Descrição": "Criação de consultas", 
 		"Data Máxima de conclusão": '2023-08-04 12:00:00', 
-		"Tempo estimado": 6
+		"Tempo estimado": '6 horas'
   },
   {
     "ID": 3,
 		"Descrição": "Otimização de banco de dados", 
 		"Data Máxima de conclusão": '2023-08-05 12:00:00', 
-		"Tempo estimado": 7
+		"Tempo estimado": '7 horas'
   },
   {
     "ID": 4,
 		"Descrição": "Desenvolvimento de sistema de autenticação", 
 		"Data Máxima de conclusão": '2023-08-06 12:00:00', 
-		"Tempo estimado": 2
+		"Tempo estimado": '2 horas'
   },
   {
     "ID": 5,
 		"Descrição": "Integração com serviços de terceiros", 
 		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
-		"Tempo estimado": 1
+		"Tempo estimado": '1 horas'
   },
   {
     "ID": 6,
 		"Descrição": "Implementação de testes unitários", 
 		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
-		"Tempo estimado": 3
+		"Tempo estimado": '3 horas'
   },
   {
     "ID": 7,
 		"Descrição": "Refatoração e melhoria de código existente", 
 		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
-		"Tempo estimado": 8
+		"Tempo estimado": '8 horas'
   },
   {
     "ID": 8,
 		"Descrição": "Análise e correção de bugs", 
 		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
-		"Tempo estimado": 4
+		"Tempo estimado": '4 horas'
   },
 ]
