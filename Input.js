e9b8a54c2b052885a5c1e98c808486ed7fab5e4f
João Victor <jvfernandes.contato@gmail.com>
2023-08-03 23:03:53 -0300

    feat: creation of array of jobs to execute

---
+++ Input.js
@@ -0,0 +1,50 @@
+const jobs = [
+  {
+    "ID": 1,
+		"Descrição": "Implantação de API", 
+		"Data Máxima de conclusão": '2023-08-04 12:00:00', 
+		"Tempo estimado": '8 horas'
+  },
+  {
+    "ID": 2,
+		"Descrição": "Criação de consultas", 
+		"Data Máxima de conclusão": '2023-08-04 12:00:00', 
+		"Tempo estimado": '6 horas'
+  },
+  {
+    "ID": 3,
+		"Descrição": "Otimização de banco de dados", 
+		"Data Máxima de conclusão": '2023-08-05 12:00:00', 
+		"Tempo estimado": '7 horas'
+  },
+  {
+    "ID": 4,
+		"Descrição": "Desenvolvimento de sistema de autenticação", 
+		"Data Máxima de conclusão": '2023-08-06 12:00:00', 
+		"Tempo estimado": '2 horas'
+  },
+  {
+    "ID": 5,
+		"Descrição": "Integração com serviços de terceiros", 
+		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
+		"Tempo estimado": '1 hora'
+  },
+  {
+    "ID": 6,
+		"Descrição": "Implementação de testes unitários", 
+		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
+		"Tempo estimado": '3 horas'
+  },
+  {
+    "ID": 7,
+		"Descrição": "Refatoração e melhoria de código existente", 
+		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
+		"Tempo estimado": '8 horas'
+  },
+  {
+    "ID": 8,
+		"Descrição": "Análise e correção de bugs", 
+		"Data Máxima de conclusão": '2023-08-10 12:00:00', 
+		"Tempo estimado": '4 horas'
+  },
+]
